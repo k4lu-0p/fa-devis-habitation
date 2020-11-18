8f507752f519346f1a617028ada7ac86c7b708f0
K4LU <lucas.rob1@live.fr>
2020-11-18 10:26:28 +0100

    - update : InputNumber component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import $axios from 'axios';
+import $moment from 'moment';
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -10,12 +11,14 @@ import $const from './constants';
 
 // Composants globaux
 import InputText from './components/Inputs/InputText';
+import InputNumber from './components/Inputs/InputNumber';
 import InputSelect from './components/Inputs/InputSelect';
 import InputAutocomplete from './components/Inputs/InputAutocomplete';
 import InputDate from './components/Inputs/InputDate';
 import InputRadioList from './components/Inputs/InputRadioList';
 
 Vue.component('input-text', InputText);
+Vue.component('input-number', InputNumber);
 Vue.component('input-select', InputSelect);
 Vue.component('input-auto-complete', InputAutocomplete);
 Vue.component('input-date', InputDate);
@@ -24,6 +27,7 @@ Vue.component('input-radio-list', InputRadioList);
 Vue.config.productionTip = false
 Vue.prototype.$const = $const;
 Vue.prototype.$axios = $axios;
+Vue.prototype.$moment = $moment;
 
 new Vue({
   router,
